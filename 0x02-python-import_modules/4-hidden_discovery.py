7e05601b4304dce3f339b01eca53ac4c191d0e7b
musaubrian <musaubrian15@gmail.com>
2022-06-02 12:41:22 -0700

    prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import hidden_4
+
+def main():
+    def vdir(hidden_4):
+        if hidden_4 == None:
+            return None
+        return [x for x in dir(hidden_4) if not x.startswith('__')]
+    print('\n'.join(str(x) for x in vdir(hidden_4)))
+
+if __name__ == '__main__':
+    main()
