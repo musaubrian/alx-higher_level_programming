3621f6d06fd7d89eeab6453341553c237eddb892
musaubrian <musaubrian15@gmail.com>
2022-06-06 12:19:10 -0700

    multiple returns

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-
     for numbers in my_list:
         print("{:d}".format(numbers))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     list_copy = my_list.copy()
-    if idx > 0:
+    if idx < 0:
         return list_copy
     elif idx > (len(my_list) - 1):
         return list_copy
