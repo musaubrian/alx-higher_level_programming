acee6cac19d4a18cbb975f820cf2f8cc9e24d6a4
musaubrian <musaubrian15@gmail.com>
2022-06-10 02:05:04 -0700

    fix bug

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,12 @@
-#!/usr.bin/python3
-
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if len(a_dictionary) == 0 or a_dictionary is dict:
         return None
 
-    return_value = list(a_ditcionary.keys())[0]
-    big_value = a_dictionary[return_value]
-    for key, value in a_dictionary.items():
-        if value > big:
-            big_value = value
-            return_value = key
-    return return_value
+    retval = list(a_dictionary.keys())[0]
+    bigval = a_dictionary[retval]
+    for k, v in a_dictionary.items():
+        if v > big:
+            bigval = v
+            retval = k
+    return retval
