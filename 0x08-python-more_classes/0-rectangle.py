5b8b8572c46127fe891bc28c3c0b5722dc9933e4
musaubrian <musaubrian15@gmail.com>
2022-06-28 05:55:25 -0700

    fix codestyle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-"""Module ``0-rectangle`` with an empty class"""
+""" Module ``0-rectangle`` with an empty class """
 
 class Rectangle:
-    """ Empty class that defines a rectangle"""
+    """ Empty class that defines a rectangle """
     pass
