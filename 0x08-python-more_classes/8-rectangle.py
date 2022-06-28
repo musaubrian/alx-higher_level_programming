da16b9041d148c5fd82c29d388fac777fe384ba9
musaubrian <musaubrian15@gmail.com>
2022-06-28 06:43:07 -0700

    fix bug

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -5,7 +5,7 @@
 
 class Rectangle:
     """
-    Defines a rectangle
+    Defines a rectangle.
     Attributes:
         number_of_instances (int): The number of instances of Rectangle.
         print_symbol (any): The symbol for string representation.
@@ -114,8 +114,8 @@ class Rectangle:
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(ret_2, Rectangle):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return (rect_1)
-        return rect_2
+        return (rect_2)
