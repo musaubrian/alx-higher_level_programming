e840b8e1223d02e5078a4f72946c1a948ea86184
musaubrian <musaubrian15@gmail.com>
2022-06-28 06:24:07 -0700

    fix documentation

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -78,12 +78,12 @@ class Rectangle:
             if h != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
+
     def __repr__(self):
         """
         Return:
-            official string represantation of rectangle
+            string represantation of rectangle
         """
-         """Return the string representation of rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return rect
