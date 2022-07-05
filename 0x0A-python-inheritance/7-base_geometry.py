97429aa0d5575e3b807894a199e6ba1b922b2c28
musaubrian <musaubrian15@gmail.com>
2022-07-05 15:01:10 +0300

    fix bug

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -27,6 +27,6 @@ class BaseGeometry:
         self.name = name
         self.value = value
         if not isinstance(self.value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError(self.name + "must be an integer")
         elif self.value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(self.name + "must be greater than 0")
