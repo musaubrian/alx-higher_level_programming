6a2e918851e6c7190ac153c093d76a3fe1d0305a
musaubrian <musaubrian15@gmail.com>
2022-07-06 13:21:00 -0700

    add 9-student
    contains a class stude that defines a student
    returns dictionary representation of a Student instance

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,12 @@
 adds all the arguments to a list and saves them to a file
 """
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
+    
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
         data = load_from_json_file("add_item.json")
