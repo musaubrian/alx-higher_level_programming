2bfc2310cadffc49dddaf869c112203ef09e7da1
musaubrian <musaubrian15@gmail.com>
2022-07-06 13:10:00 -0700

    add 8-class_to_json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,6 +8,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
+
     try:
         data = load_from_json_file("add_item.json")
     except FileNotFoundError:
