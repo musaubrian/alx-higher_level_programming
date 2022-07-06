d83e9db190c4b29e4203a212eb9f0e616fb1691b
musaubrian <musaubrian15@gmail.com>
2022-07-06 11:54:42 -0700

    add 1-write_file
    contains function write_file()
    which writes text to a file
    both text and file are passed as arguments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,4 +11,4 @@ def read_file(filename=""):
     """
     with open(filename, "r", encoding="utf-8") as file:
         for line in file.readlines():
-            print(line)
+            print(line.rstrip())
