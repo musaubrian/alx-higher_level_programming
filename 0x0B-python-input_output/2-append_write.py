991031377e48357628b6711f6d4dd521efb31f1e
musaubrian <musaubrian15@gmail.com>
2022-07-06 12:00:59 -0700

    Add 2-append_write
    The file contains a function that appends text to a file
    ()at the end)

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module contains the function `append_write()`
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    the functions appends text to an existing file
+    Args:
+        @filename: the name of the file
+        @text: text being manipulated
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        no_of_characters = file.write(text)
