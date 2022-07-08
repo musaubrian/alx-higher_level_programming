027dd61c6274386c5ceb62a141e2edd07073ac9f
musaubrian <musaubrian15@gmail.com>
2022-07-08 11:12:37 +0300

    add validation for all setter methods
    check if values in setters are ints or less than 0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,6 +35,10 @@ class Rectangle(BaseClass):
     @width.setter
     def width(self, value):
         """setter method for width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -49,6 +53,10 @@ class Rectangle(BaseClass):
     @height.setter
     def height(self, value):
         """setter method for height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be > 0")
         self.__height = value
     
     @property
@@ -63,6 +71,8 @@ class Rectangle(BaseClass):
     @x.setter
     def x(self, value):
         """setter method for x attribute"""
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -77,4 +87,6 @@ class Rectangle(BaseClass):
     @y.setter
     def y(self, value):
         """setter method for y attribute"""
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
