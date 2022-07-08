a959bb5841150f433963cd41080d4f466d4f1f1d
musaubrian <musaubrian15@gmail.com>
2022-07-08 09:58:14 +0300

    add setter and getter methods
    create private attributes each with its own
    public getter && setter for the class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,3 +25,56 @@ class Rectangle(BaseClass):
         self.height = height
         self.x = x
         self.y = y
+
+
+    @property
+    def width(self):
+        """getter method for width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter method for width attribute"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        getter method for height attribute
+        return:
+            height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter method for height attribute"""
+        self.__height = value
+    
+    @property
+    def x(self):
+        """
+        getter method for x attribute
+        return:
+            x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter method for x attribute"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        getter method for y attribute
+        return:
+            y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter method for y attribute"""
+        self.__y = value
