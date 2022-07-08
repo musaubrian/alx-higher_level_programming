dd327fb65c25abd7fd9e87771d1304af309dfbd4
musaubrian <musaubrian15@gmail.com>
2022-07-08 09:10:00 +0300

    initialize Class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
+"""
+module `base` contains class base
+"""
 
+
+class Base:
+    """
+    Base of all other classes
+    Manages `id` attribute of all other classes
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        initialize class instance
+        Args:
+            @id: value of each new class instance
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
