25436ef95120a814a210fba060148544b8fcf5df
musaubrian <musaubrian15@gmail.com>
2022-07-10 18:21:29 +0300

    update class Base
    add seave_to_file and to_json_string method
    which write to a json file the class instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 """
 module `base` contains class base
 """
-
+import json
 
 class Base:
     """
@@ -23,3 +23,33 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        return:
+            JSON representation of an object string
+        Args:
+            list_dictionary: list of dictionaries(list)
+        """
+        if list_dictionaries is None:
+            return "[]"
+
+        json_dump = json.dumps(list_dictionaries)
+        return json_dump
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file
+        Args:
+            @list_objs: (list) list of instances inheriting Base
+        """
+
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(Base.to_json_string(list_dicts))
