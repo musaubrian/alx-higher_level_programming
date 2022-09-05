6bc3fc949a3d6db23ffe7c965b7cf52f2b0fee89
musaubrian <musaubrian15@gmail.com>
2022-09-05 16:38:12 +0300

    add: loop through an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let index = 0; index < myArr.length; index++) {
+  const element = myArr[index];
+  console.log(element);
+}
