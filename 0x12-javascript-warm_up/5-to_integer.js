c8658f17709e6e55a161b72845a4693a62ed63e8
musaubrian <musaubrian15@gmail.com>
2022-09-05 16:32:19 +0300

    add: convert to int if possible

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const numToParse = parseInt(process.argv[2]);
 if (isNaN(numToParse)) {
   console.log('Not a number');
 } else {
-  console.log(numToParse);
+  console.log('My number: ' + numToParse);
 }
