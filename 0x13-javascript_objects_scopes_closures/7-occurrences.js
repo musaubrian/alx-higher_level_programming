94638cf49563e3722fa3b81b85283a334b1d5e02
musaubrian <musaubrian15@gmail.com>
2022-09-07 09:55:25 +0300

    format: follow semistandard cosestyle

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  counter = 0;
+  let counter = 0;
   for (let index = 0; index < list.length; index++) {
-    if (list[index] == searchElement) {
+    if (list[index] === searchElement) {
       counter += 1;
     }
   }
