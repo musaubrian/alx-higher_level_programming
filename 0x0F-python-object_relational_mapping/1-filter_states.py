f2b849c654378f1efa4a5a56263e191db8538df2
musaubrian <musaubrian15@gmail.com>
2022-09-11 13:34:00 +0300

    update: fix errors in filter_states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-python script that lists all states from the database hbtn_0e_0_usa
+python script that lists all states from starting with N
 """
 
 import MySQLdb
@@ -11,9 +11,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cursor.close()
     db.close()
