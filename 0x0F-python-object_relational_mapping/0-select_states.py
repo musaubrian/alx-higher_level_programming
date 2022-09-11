736fb0bd230b932631798563d4040ed5b09cb07e
musaubrian <musaubrian45@gmail.com>
2022-09-11 12:17:00 +0300

    add: list states in the db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import MySQLdb
+import sys
+
+def list_states(username, password, db_name):
+    """
+    lists all the states in the database
+    """
+    database = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=db_name,
+            port=3306
+            )
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM `states` ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    database.close()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    list_states(username=username, password=password, db_name=db_name)
