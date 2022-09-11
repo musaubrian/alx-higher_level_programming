35787605d7675da09ae2cc1b160af3760afd3909
musaubrian <musaubrian15@gmail.com>
2022-09-11 12:56:25 +0300

    add: filter states by user input

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -20,14 +20,13 @@ def filter_states(username, password, db_name):
                    WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
-        if ("N" in row[1]):
+        if row[1][0] == "N":
             print(row)
     cursor.close()
     db.close()
 
 
 if __name__ == "__main__":
-    credentials = sys.argv
     username = sys.argv[1]
     passwd = sys.argv[2]
     db_name = sys.argv[3]
