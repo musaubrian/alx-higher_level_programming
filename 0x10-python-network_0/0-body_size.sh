ea5a482771d41c1420f8f33050215af5e0d4dcb0
musaubrian <musaubrian15@gmail.com>
2022-09-22 15:35:16 +0300

    update: remove extra code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #displays the size of the body of the response
-curl -sIw '%{size_download}' "$1" | grep "Content-Length:"| cut -d":" -f2
-
+curl -sI "$1" | grep "Content-Length:"| cut -d":" -f2
