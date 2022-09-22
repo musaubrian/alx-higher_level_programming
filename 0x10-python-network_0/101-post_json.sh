916977e09e83c1cfeea1d1a6671f0f157acfc15e
Ernest <94367979+musaubrian@users.noreply.github.com>
2022-09-22 16:42:11 +0300

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a POST request with the contents of a file
-curl -s -X POST --header 'Content-Type: application/json' --data @"$2" "$1"
+curl -s -X POST -H 'Content-Type: application/json' -d @"$2" "$1"
