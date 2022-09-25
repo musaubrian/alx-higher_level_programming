f413a54fe00ab9c26a47d333e9164f2bfe8ead30
musaubrian <musaubrian15@gmail.com>
2022-09-25 15:58:54 +0300

    add: get github user id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+display users github id
+"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    headers = {'Authentization': 'Basic ' + '{}:{}'.format(username, password)}
+    headers['Accept'] = 'application/json'
+    r = requests.post(url, headers=headers)
+
+    if r.status_code == 200:
+        data = r.json()
+    else:
+        print("None")
