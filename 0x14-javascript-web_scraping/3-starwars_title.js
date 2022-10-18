fa5d3dae57ae3bb92d455786c451e115cdde03ca
musaubrian <musaubrian45@gmail.com>
2022-10-18 17:37:51 +0300

    Refactor: follow code style

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
-// prints the title of a Star Wars movie 
+// prints the title of a Star Wars movie
 // where the episode number matches a given integer.
 
 const request = require('request');
 const episodeNumber = process.argv[2];
-const apiUrl = "https://swapi-api.hbtn.io/api/films/";
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
 
 request(apiUrl + episodeNumber, function (error, response, body) {
-    if (error) {
-        throw error;
-    } else if (response.statusCode === 200) {
-        const jsonResponse = JSON.parse(body);
-        console.log(jsonResponse.title);
-    } else {
-        console.log('Error code: ' + response.statusCode);
-    }
-})
+  if (error) {
+    throw error;
+  } else if (response.statusCode === 200) {
+    const jsonResponse = JSON.parse(body);
+    console.log(jsonResponse.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
