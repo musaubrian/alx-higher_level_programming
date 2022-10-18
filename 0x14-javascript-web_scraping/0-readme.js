9bea24fde1a960e9b9214f7ad15b16c285dd1c11
musaubrian <musaubrian45@gmail.com>
2022-10-18 17:01:44 +0300

    Chore: read from file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/bin/node
+
+//reads and prints content of a file
+// passed as an argument
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data);
+    }
+});
